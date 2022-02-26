e49e1b235a84109a7f111c6def0662be29a4a791
Kamil Wrobel <wrobelekpl@hotmail.com>
2022-02-25 22:16:51 -0500

    added tests

--- HackerRank.playground/Pages/Arrays Roatate arr.xcplaygroundpage/Contents.swift
+++ HackerRank.playground/Pages/Arrays Roatate arr.xcplaygroundpage/Contents.swift
@@ -20,3 +20,32 @@ func rotLeft(a: [Int], d: Int) -> [Int] {
 
 
 
+
+import XCTest
+
+class ArrayRotationTests: XCTestCase {
+    
+    func testOne(){
+        let input = [33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97]
+        let output = [87, 97, 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60]
+        XCTAssertEqual(rotLeft(a: input, d: 13), output, "Failed on test one")
+    }
+    
+    func testTwo(){
+        let input = [41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51]
+        let output = [77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77]
+        XCTAssertEqual(rotLeft(a: input, d: 10), output, "Failed on test two")
+    }
+    
+    func testThree(){
+        let input = [1, 2, 3, 4, 5,]
+        let output = [5, 1, 2, 3, 4]
+        XCTAssertEqual(rotLeft(a: input, d: 4), output, "Failed on test three")
+    }
+
+    
+    
+}
+
+
+ArrayRotationTests.defaultTestSuite.run()
